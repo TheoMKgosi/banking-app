47a2f0103a7943449727ca018b8770095e419e9b
Theo <theo.kgosiemang12@gmail.com>
2024-06-26 10:38:02 +0000

    Scaffold

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,15 @@
+//import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h2> Saccos logo</h2>
+      <form>
+        <label>Email</label>
+        <input></input>
+        <label>Password</label>
+        <input></input>
+      </form>
+    </div>
+  );
+}
