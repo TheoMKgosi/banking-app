7f40cabdedb631d90ff46e77ca92056ef4915f31
TheoMKgosi <theo.kgosiemang12@gmail.com>
2025-05-20 22:26:59 +0200

    Restarting to beginning

--- backend/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return ['Jwaneng saccos' => app()->version()];
-});
-
-require __DIR__.'/auth.php';
--- frontend/src/components/RedButton.js
+++
@@ -1,9 +0,0 @@
-const RedButton = ({ type = 'submit', className, ...props }) => (
-    <button
-        type={type}
-        className={`${className} inline-flex items-center rounded-md border border-transparent bg-red-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white ring-red-300 transition duration-150 ease-in-out hover:bg-red-700 focus:border-red-900 focus:outline-none focus:ring active:bg-red-900 disabled:opacity-25`}
-        {...props}
-    />
-);
-
-export default RedButton;
